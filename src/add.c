e375928ae1f3bdca2f8604c361d2fa482489d8db
Mallikarjun <gudmallu@gmail.com>
2010-07-09 14:57:28 +0530

    once again trying to push

--- src/add.c
+++ src/add.c
@@ -6,3 +6,8 @@ sjkfjkdsjkf
 
 added by mallu 
 conflicts merged
+
+trying to push to master once again.
+I am in mallu_1 branch changed the file 
+need to add and commit then merge to master then push.
+this things i am going to do in gui
